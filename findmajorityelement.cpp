4ea2dc0fc1229a890f0d7f559d679a87064168ee
suryadevsingh123 <51847197+suryadevsingh123@users.noreply.github.com>
2021-02-17 11:25:23 +0530

    Update findmajorityelement.cpp

--- findmajorityelement.cpp
+++ findmajorityelement.cpp
@@ -1,38 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int findmaj(int arr[],int m){
-	int n=m/2;
-	int temp(-1),mcount(0);
-	for(int i=0;i<m;i++){
-		int count=0;
-		for(int j=0;j<m;j++){
-			if(arr[i]==arr[j]){
-				count++;
-			}
-			if(mcount<count){
-				mcount=count;
-				temp=i;
-			}
-		}
-	}
-		if(mcount>n){
-			cout<<arr[temp]<<"\n";
-		}
-		else{
-			cout<<"Do not exits"<<"\n";
-		}
-
-}
  int main(){
- 	int t;
- 	cin>>t;
- 	while(t--){
+ 
  		int m;
  		cin>>m;
  		int arr[m];
  		for(int i=0;i<m;i++){
  			cin>>arr[i];
-		 }
-		findmaj(arr,m);
-	 }
- }
+        }
+        int count,count1=0,index(-1);
+        for(int i=0; i<m; i++)
+        {
+        	count=0;
+        for(int j=0;j<m;j++)
+        {
+        	if(arr[i]==arr[j])
+        	{
+        		count++;
+			}
+			if(count1<count){
+				count1=count;
+				index=i;
+			}
+		}
+	}
+		if(count1>=m/2)
+		{
+			cout<<arr[index];
+		}
+		else
+		{
+			cout<<"no majority element";
+		}
+}
